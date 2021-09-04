ec15b3e961e1c222573e92c200b991be42c32caa
weiseng <weiseng@i-neighbour.com>
2021-09-04 13:59:47 +0800

    Initial Commit

---
+++ Area Detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Area Detection
+//
+//  Created by 2018MAC04 on 04/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
